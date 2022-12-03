2df45e367ff3481e3585272c34f88dc38e81dcd5
Ben Heidemann <ben@heidemann.co.uk>
2022-12-03 22:19:29 +0000

    tidyup

--- src/main/java/net/fabricmc/whereis/LocationFile.java
+++ src/main/java/net/fabricmc/whereis/LocationFile.java
@@ -11,19 +11,18 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.minecraft.util.math.Vec3d;
 
 public class LocationFile {
-  private Logger LOGGER;
+  private static final Logger LOGGER = LoggerFactory.getLogger("whereis");
   private String path = null;
   private JSONArray locations = null;
 
   // Constructor
-  public LocationFile(String path, Logger logger) throws IOException, ParseException {
-    this.LOGGER = logger;
+  public LocationFile(String path) throws IOException, ParseException {
     this.path = path;
-
     this.load();
   }
   
--- src/main/java/net/fabricmc/whereis/WhereIs.java
+++ src/main/java/net/fabricmc/whereis/WhereIs.java
@@ -2,7 +2,6 @@ package net.fabricmc.whereis;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.entity.Entity;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
@@ -22,30 +21,24 @@ import org.json.simple.parser.ParseException;
 import static net.minecraft.server.command.CommandManager.*;
 
 public class WhereIs implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("whereis");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Starting WhereIs mod...");
 
 		LocationFile locationFile;
-
 		try {
-			locationFile = new LocationFile("whereis.locations.json", LOGGER);
+			locationFile = new LocationFile("whereis.locations.json");
 		}
 		catch (IOException e) {
-			LOGGER.error("Failed to create or load the location file");
+			LOGGER.error("Failed to load locations from whereis.locations.json: " + e.getMessage());
+			e.printStackTrace();
 			return;
 		}
 		catch (ParseException e) {
-			LOGGER.error("Failed to parse location file");
+			LOGGER.error("Failed to load locations from whereis.locations.json: " + e.getMessage());
+			e.printStackTrace();
 			return;
 		}
 
@@ -61,12 +54,7 @@ public class WhereIs implements ModInitializer {
 									.executes(context -> {
 										ServerCommandSource source = context.getSource();
 										String alias = StringArgumentType.getString(context, "alias");
-
-										LOGGER.info("Looking up location " + alias);
-
 										ArrayList<Location> foundLocations = locationFile.findLocations(alias);
-
-										LOGGER.info("Done");
 
 										if (foundLocations.size() == 0) {
 											source.sendError(Text.literal("No locations found"));
@@ -135,7 +123,8 @@ public class WhereIs implements ModInitializer {
 											locationFile.addLocation(alias, position);
 										}
 										catch (IOException e) {
-											LOGGER.error("Failed to write to location file");
+											LOGGER.error("Failed to write to location file: " + e.getMessage());
+											e.printStackTrace();
 											String errorMessage = "Failed to save location \"" + alias + "\"";
 											source.sendError(Text.literal(errorMessage));
 											return -1;
@@ -147,11 +136,8 @@ public class WhereIs implements ModInitializer {
 							.executes(context -> {
 								context
 									.getSource()
-									.sendFeedback(
-										Text
-											.literal("Usage: /hereis <alias>")
-											.setStyle(MessageStyle),
-										false
+									.sendError(
+										Text.literal("Usage: /hereis <alias>")
 									);
 
 								return 1;
